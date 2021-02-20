53062dfcbcdd1c21059c405bb999e292f07fadde
Vlad <vladislav0000054@gmail.com>
2021-02-20 11:46:48 +0700

    Добавлен SimpleLog

---
+++ ServerCourse/SimpleLog/Car.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace SimpleLog
+{
+    public class Car
+    {
+        public string Model { get; set; }
+
+        public double MaxSpeed { get; set; }
+
+        public double Price { get; set; }
+
+        public double CurrentSpeed { get; set; }
+
+        private readonly ILogger<Car> _logger;
+
+        public Car(double price, double maxSpeed, string model, ILogger<Car> logger)
+        {
+            Price = price;
+            MaxSpeed = maxSpeed;
+            Model = model;
+            _logger = logger;
+        }
+
+        public void Go()
+        {
+            CurrentSpeed += 10;
+            _logger.LogDebug(20, $"Текущая скорость: {CurrentSpeed}");
+        }
+    }
+}
---
+++ ServerCourse/SimpleLog/SimpleLog.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using LogLevel = Microsoft.Extensions.Logging.LogLevel;
+
+using NLog;
+using NLog.Extensions.Logging;
+
+namespace SimpleLog
+{
+    internal static class SimpleLog
+    {
+        private static void Main()
+        {
+            var logger = LogManager.GetCurrentClassLogger();
+            try
+            {
+                var config = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                    .Build();
+
+                var servicesProvider = BuildDi(config);
+                using (servicesProvider as IDisposable)
+                {
+                    var mazda = servicesProvider.GetRequiredService<Car>();
+                    mazda.Go();
+
+                    Console.WriteLine("Press ANY key to exit");
+                    Console.ReadKey();
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "Stopped program because of exception");
+                throw;
+            }
+            finally
+            {
+                LogManager.Shutdown();
+            }
+        }
+
+        private static IServiceProvider BuildDi(IConfiguration config)
+        {
+            return new ServiceCollection()
+                .AddTransient<Car>()
+                .AddLogging(loggingBuilder =>
+                {
+                    loggingBuilder.ClearProviders();
+                    loggingBuilder.SetMinimumLevel(LogLevel.Trace);
+                    loggingBuilder.AddNLog(config);
+                })
+                .BuildServiceProvider();
+        }
+    }
+}
