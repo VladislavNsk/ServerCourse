c43f1e3c2103c30f4177f2d026b29e0421ff08da
Vlad <vladislav0000054@gmail.com>
2021-02-21 13:05:50 +0700

    Форматирование кода

--- ServerCourse/Json/Json.cs
+++ ServerCourse/Json/Json.cs
@@ -14,9 +14,8 @@ namespace Json
         private static async Task Main()
         {
             var client = new HttpClient();
-            var respons = await client.GetAsync("https://restcountries.eu/rest/v2/region/americas");
-            var json = respons.Content.ReadAsStringAsync().Result;
-            var countries = JsonConvert.DeserializeObject<Country[]>(json);
+            var response = await client.GetAsync("https://restcountries.eu/rest/v2/region/americas");
+            var countries = JsonConvert.DeserializeObject<Country[]>(await response.Content.ReadAsStringAsync());
 
             var totalPopulation = countries.Sum(c => c.Population);
             Console.WriteLine($"Общая численность населения: {totalPopulation}");
